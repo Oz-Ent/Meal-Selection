42c619ff63b967af65dde630490d8c59ebac9c4e
Hubert Kingsley Ocran <hubertkingsley.ocran.ext@heidelbergmaterials.com>
2026-03-17 12:48:09 +0000

    Change router test to jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as util from 'util';
+
+if (typeof (globalThis as any).TextEncoder === 'undefined') {
+  (globalThis as any).TextEncoder = util.TextEncoder;
+  (globalThis as any).TextDecoder = util.TextDecoder;
+}
