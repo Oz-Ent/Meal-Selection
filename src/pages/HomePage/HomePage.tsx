984f5a372c3ac49a430795e462147d5fe4168bde
Hubert Kingsley Ocran <hubertkingsley.ocran.ext@heidelbergmaterials.com>
2026-05-24 22:14:47 +0000

    Complete user pages

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,6 @@
 
 
 export default function HomePage() {
-    const options =[
-        {label:'Red' , value: ' 0'},
-        {label:'Blue' , value: ' 1'},
-        {label:'Green' , value: ' 2'},
-    ]
     return (
         <div>Home Page</div>
     );
