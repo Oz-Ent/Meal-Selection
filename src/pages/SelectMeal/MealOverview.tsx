aba7d6040f3ad0a3716ad74cea52c47d0ec7bacc
Hubert Kingsley Ocran <hubertkingsley.ocran.ext@heidelbergmaterials.com>
2026-07-02 14:35:37 +0000

    Add key to meal overview

--- src/pages/SelectMeal/MealOverview.tsx
+++ src/pages/SelectMeal/MealOverview.tsx
@@ -1,24 +1,28 @@
-import Modal from "../../components/Modal/Modal";
-import { availableMeals } from "../../helpers/availableMeals";
-import ListCard from "../../components/ListCard/ListCard";
+import Modal from '../../components/Modal/Modal';
+import { availableMeals } from '../../helpers/availableMeals';
+import ListCard from '../../components/ListCard/ListCard';
 
 export function MealOverview({ selectedMeals }: { selectedMeals: Record<string, string> }) {
-    return <Modal isOpen={true} onClose={() => { }} variant={"bottom"}>
-        <div className="h-[80vh] overflow-y-auto px-3 py-2">
-            <div>
-                <h2 className="text-[18px] font-semibold mb-2 text-msTextPrimary">My Meals For The Week</h2>
-            </div>
-            {Object.entries(selectedMeals).map(([day, meal]) => (
-                <div>
-                    <p className="text-[16px] font-medium mt-1 text-msTextPrimary">{day}</p>
-                    <ListCard
-                        title={availableMeals.find((item) => item.id === meal)?.title || ""}
-                        imageUrl={availableMeals.find((item) => item.id === meal)?.imageUrl || ""}
-                        id={day}
-                        inputType="none"
-                    />
-                </div>
-            ))}
-        </div> 
+  return (
+    <Modal isOpen={true} onClose={() => {}} variant={'bottom'}>
+      <div className="h-[80vh] overflow-y-auto px-3 py-2">
+        <div>
+          <h2 className="text-[18px] font-semibold mb-2 text-msTextPrimary">
+            My Meals For The Week
+          </h2>
+        </div>
+        {Object.entries(selectedMeals).map(([day, meal]) => (
+          <div key={day}>
+            <p className="text-[16px] font-medium mt-1 text-msTextPrimary">{day}</p>
+            <ListCard
+              title={availableMeals.find((item) => item.id === meal)?.title || ''}
+              imageUrl={availableMeals.find((item) => item.id === meal)?.imageUrl || ''}
+              id={day}
+              inputType="none"
+            />
+          </div>
+        ))}
+      </div>
     </Modal>
+  );
 }
