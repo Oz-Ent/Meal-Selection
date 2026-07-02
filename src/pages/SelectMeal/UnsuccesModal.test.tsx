aecd56cd5f2ddbf0413a4b1f895deb73c87e0f75
Hubert Kingsley Ocran <hubertkingsley.ocran.ext@heidelbergmaterials.com>
2026-07-02 16:43:38 +0000

    Fix modals test

--- src/pages/SelectMeal/UnsuccesModal.test.tsx
+++ src/pages/SelectMeal/UnsuccesModal.test.tsx
@@ -2,12 +2,14 @@ import { render, screen } from '@testing-library/react';
 import { UnsuccessModal } from './UnsuccesModal';
 
 describe('UnsuccessModal Component', () => {
-    it('renders the unsuccess message and buttons', () => {
-        render(<UnsuccessModal />);
-        expect(screen.getByText('Unsuccessful')).toBeInTheDocument();
-        expect(screen.getByText(/The meals you have chosen for the week was not recorded successfully/i)).toBeInTheDocument();
-        
-        expect(screen.getByRole('button', { name: 'Try Again' })).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: 'Back To Choose Meals' })).toBeInTheDocument();
-    });
+  it('renders the unsuccess message and buttons', () => {
+    render(<UnsuccessModal />);
+    expect(screen.getByText('Unsuccessful')).toBeInTheDocument();
+    expect(
+      screen.getByText(/The meals you have chosen for the week were not recorded successfully/i),
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('button', { name: 'Try Again' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Back To Choose Meals' })).toBeInTheDocument();
+  });
 });
