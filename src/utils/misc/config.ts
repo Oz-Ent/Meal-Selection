6a61f6da6f3449375023c333c0cb239cc512bd86
Hubert Kingsley Ocran <kingsleyocran94@gmail.com>
2026-08-29 11:22:22 +0000

    Change url to prod

--- src/utils/misc/config.ts
+++ src/utils/misc/config.ts
@@ -1,2 +1,2 @@
 export const MEAL_APP_CORE =
-  import.meta.env.VITE_MEAL_APP_CORE ?? 'https://meal-app-core-dev.vercel.app';
+  import.meta.env.VITE_MEAL_APP_CORE ?? 'https://meal-app-core.vercel.app';
