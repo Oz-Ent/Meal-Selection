0709b3592d893db10ac577b7ed891a29b39862ad
Hubert Kingsley Ocran <kingsleyocran94@gmail.com>
2026-09-16 10:09:40 +0000

    Fix keep me signed in logic

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,14 @@ import vitePluginSvgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vitePluginSvgr(),react(), tailwindcss()],
+  plugins: [vitePluginSvgr(), react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api/core": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/core/, ""),
+      },
+    },
+  },
 });
